1fea812afbd9351f982f83b42f26373cd4f0b91f
Karthik Saxena <katik.ks@gmail.com>
2021-03-10 18:05:31 +0530

    angularapp

---
+++ ngapp/first/src/app/category/category.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+//Decorator
+@Component({
+    selector:'[category]',
+    templateUrl:'./category.component.html',
+    styleUrls:['./category.component.css']
+})
+
+export class CategoryComponent {
+
+}
---
+++ ngapp/first/src/app/navbar/navbar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+//Decorator
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+})
+export class NavbarComponent {}
