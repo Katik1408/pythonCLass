8b88b6a82280a137587e6c73dd14d7e51d710c67
Karthik Saxena <katik.ks@gmail.com>
2021-03-15 18:14:12 +0530

    15032021

--- ngapp/first/src/app/app.component.ts
+++ ngapp/first/src/app/app.component.ts
@@ -3,8 +3,44 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'first';
+  title: string = 'first';
+  num: number = 1233;
+
+  isActive: boolean = true;
+
+  placeholderData = 'Inside Placeholder';
+
+  getData() {
+    return 'Some value';
+  }
+
+  changeTitle() {
+    this.title = 'New title';
+  }
+
+  students = [
+    {
+      Name: 'Rahul',
+      Age: 25,
+      Place: 'Bangalore',
+    },
+    {
+      Name: 'Rohit',
+      Age: 28,
+      Place: 'Noida',
+    },
+    {
+      Name: 'Rohan',
+      Age: 29,
+      Place: 'Delhi',
+    },
+    {
+      Name: 'Harsh',
+      Age: 31,
+      Place: 'Chennai',
+    },
+  ];
 }
--- ngapp/first/src/app/app.module.ts
+++ ngapp/first/src/app/app.module.ts
@@ -3,18 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {CategoryComponent} from 'src/app/category/category.component';
-import { NavbarComponent } from './navbar/navbar.component'
+import { CategoryComponent } from 'src/app/category/category.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,CategoryComponent, NavbarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CategoryComponent, NavbarComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
